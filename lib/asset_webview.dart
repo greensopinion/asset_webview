d13493b6a3daf4d1ce80fec27c55bcb338d3904a
David Green <greensopinion@gmail.com>
2021-05-08 09:13:08 -0700

    null migration

--- lib/asset_webview.dart
+++ lib/asset_webview.dart
@@ -1,14 +1,11 @@
-
 import 'dart:async';
 
 import 'package:flutter/services.dart';
 
 class AssetWebview {
-  static const MethodChannel _channel =
-      const MethodChannel('asset_webview');
+  static const MethodChannel _channel = const MethodChannel('asset_webview');
 
   static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
+    return await _channel.invokeMethod('getPlatformVersion') ?? "<unknown>";
   }
 }
